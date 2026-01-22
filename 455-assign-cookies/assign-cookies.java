fd105bfdcf717d57c937c2374f6831a617cf609c
Devang Choudhary <devangchoudhary71@gmail.com>
2026-01-23 00:43:31 +0530

    Time: 15 ms (61.78%) | Memory: 51.8 MB (12.76%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+     int i=0;
+     int j=0;
+     int cnt=0;
+     Arrays.sort(g);
+     Arrays.sort(s);
+     while(i<g.length && j<s.length){
+        if(s[j]>=g[i]){
+            i++;
+            j++;
+            cnt++;
+        }
+        else{
+            j++;
+        }
+     }
+     return cnt;
+    }
+}
