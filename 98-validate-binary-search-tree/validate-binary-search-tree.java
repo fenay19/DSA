af1687b4b3e812bcd1250e1f37175dba0a69f550
Devang Choudhary <devangchoudhary71@gmail.com>
2026-03-26 01:45:14 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (86.22%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,14 +14,29 @@
  * }
  */
 class Solution {
+    TreeNode prev=null;
+    boolean ans=true;
     public boolean isValidBST(TreeNode root) {
-      return  helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+      return  helper(root);
     }
-    public boolean helper(TreeNode root,long min,long max){
+    public boolean helper(TreeNode root){
         if(root==null) return true;
 
-        if(root.val<=min || root.val>=max) return false;
+      helper(root.left);
 
-        return helper(root.left,min,root.val) && helper(root.right,root.val,max);
+if(prev==null) {
+    prev=root;
+}
+else if(prev!=null && prev.val>=root.val){
+ans=false;
+
+}
+prev=root;
+
+
+helper(root.right);
+
+
+return ans;
     }
 }
