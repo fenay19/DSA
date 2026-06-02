74e80572643ee3e06f75a96ef10e2b932b2faea4
Devang Choudhary <devangchoudhary71@gmail.com>
2026-06-02 19:52:21 +0530

    Time: 9 ms (85.82%) | Memory: 46.8 MB (12.92%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,58 +1,65 @@
 class Solution {
-    public int swimInWater(int[][] heights) {
-           int n = heights.length;
-        int m = heights[0].length;
+    public int swimInWater(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
 
-        int[][] res = new int[n][m];
+        int l=grid[0][0];
+        int h=grid[0][0];
 
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(res[i], Integer.MAX_VALUE);
-        }
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> a[0] - b[0]
-        );
-
-        int[] x = {1, -1, 0, 0};
-        int[] y = {0, 0, -1, 1};
-
-        res[0][0] = heights[0][0];
-
-        pq.offer(new int[]{heights[0][0], 0, 0});
-
-        while (!pq.isEmpty()) {
-
-            int[] curr = pq.poll();
-
-            int dist = curr[0];
-            int r = curr[1];
-            int c = curr[2];
-
-            if (dist > res[r][c]) {
-                continue;
-            }
-
-            for (int k = 0; k < 4; k++) {
-
-                int r1 = r + x[k];
-                int c1 = c + y[k];
-
-                if (r1 >= 0 && r1 < n && c1 >= 0 && c1 < m) {
-
-                 
-
-                    int newdist = Math.max(heights[r1][c1], dist);
-
-                    if (newdist < res[r1][c1]) {
-
-                        res[r1][c1] = newdist;
-
-                        pq.offer(new int[]{newdist, r1, c1});
-                    }
-                }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                h=Math.max(h,grid[i][j]);
             }
         }
 
-        return res[n - 1][m - 1];
+        int res=0;
+        while(l<=h){
+            int v=l+(h-l)/2;
+
+     if(bfs(grid,n,m,v)){
+ res=v;
+ h=v-1;
+     }
+     else{
+
+        l=v+1;
+     }
+        }
+        return res;
     }
+
+         boolean bfs(int[][] a,int n,int m,int v){
+            if(a[0][0]>v) return false;
+        Queue<int []>q=new LinkedList<>();
+        boolean[][] vis = new boolean[n][m];
+        vis[0][0]=true;
+        int[] x={1,-1,0,0};
+        int[] y={0,0,1,-1};
+        q.offer(new int [] {0,0});
+
+        while(!q.isEmpty()){
+            int [] curr=q.poll();
+            int r=curr[0];
+            int c=curr[1];
+            if(r==n-1 && c==m-1){
+                return true;
+            }
+for(int i=0;i<4;i++){
+  int  r1=r+x[i];
+  int  c1=c+y[i];
+
+            if(r1>=0 && r1<n && c1>=0 &&c1<m && vis[r1][c1]==false && v>=a[r1][c1] ){
+
+                q.offer(new int [] {r1,c1});
+
+                vis[r1][c1]=true;
+               
+            }
+        }
+        }
+
+return false;
+
+        }
+    
 }
