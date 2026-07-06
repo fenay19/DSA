7e8e0ed6be7b625d5779e29a8d4c033494c5273c
Devang Choudhary <devangchoudhary71@gmail.com>
2026-07-07 00:36:11 +0530

    Time: 0 ms (100.00%) | Memory: 50.1 MB (65.79%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int hIndex(int[] c) {
+        int l=0;
+        int h=c.length-1;
+        int n=c.length;
+        int res=0;
+        while(l<=h){
+int m=(l+h)/2;
+
+if(c[m]>=n-m){
+    res=n-m;
+    h=m-1;
+}
+else{
+    l=m+1;
+}
+
+
+
+        }
+        return res;
+    }
+}
