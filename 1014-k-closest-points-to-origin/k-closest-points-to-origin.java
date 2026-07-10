49b1b81399b329b6cfd33fc154c7f27952e38b26
Devang Choudhary <devangchoudhary71@gmail.com>
2026-07-10 21:58:19 +0530

    Time: 32 ms (37.88%) | Memory: 58.8 MB (20.78%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,37 +1,29 @@
-import java.util.*;
-
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
 
-        // Max heap based on distance
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
             (a, b) -> b[0] - a[0]
         );
 
+        int[][] res = new int[k][2];
+
         for (int i = 0; i < points.length; i++) {
+
             int x = points[i][0];
             int y = points[i][1];
 
             int dist = x * x + y * y;
 
-            // store {distance, index}
-            maxHeap.offer(new int[]{dist, i});
+            pq.offer(new int[]{dist, i});
 
-            if (maxHeap.size() > k) {
-                maxHeap.poll(); // remove farthest
+            if (pq.size() > k) {
+                pq.poll();
             }
         }
 
-        int[][] res = new int[k][2];
-        int i = 0;
-
-        while (!maxHeap.isEmpty()) {
-            int[] ele = maxHeap.poll();
-            int idx = ele[1];
-
-            res[i][0] = points[idx][0];
-            res[i][1] = points[idx][1];
-            i++;
+        for (int i = 0; i < k; i++) {
+            int[] curr = pq.poll();
+            res[i] = points[curr[1]];
         }
 
         return res;
