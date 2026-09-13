a7fdac4400a74c6aec14b46add45cdd2065f43d5
Devang Choudhary <devangchoudhary71@gmail.com>
2026-09-14 01:02:02 +0530

    Time: 72 ms (65.84%) | Memory: 47.1 MB (88.97%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -1,20 +1,50 @@
+import java.util.PriorityQueue;
+
 class Solution {
-    public int rangeSum(int[] nums, int n, int left, int right) {
-          int MOD = 1_000_000_007;
-     ArrayList<Integer> arr =new ArrayList<>();
-for(int i=0;i<n;i++){
-    int sum=0;
-    for(int j=i;j<n;j++){
-        sum+=nums[j];
-        arr.add(sum);
+
+    static class Node {
+        long sum;
+        int index;
+
+        Node(long sum, int index) {
+            this.sum = sum;
+            this.index = index;
+        }
     }
 
-}
-int sum1=0;
-arr.sort(Comparator.naturalOrder()); 
-for(int i=left-1;i<right;i++){
-sum1=(sum1+arr.get(i))%MOD;
-}
-return (int) sum1;
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        int MOD = 1_000_000_007;
+
+        PriorityQueue<Node> mh = new PriorityQueue<>(
+            (a, b) -> Long.compare(a.sum, b.sum)
+        );
+
+        // Initially add all single-element subarrays
+        for (int i = 0; i < n; i++) {
+            mh.offer(new Node(nums[i], i));
+        }
+
+        long res = 0;
+
+        for (int i = 0; i < right; i++) {
+            Node curr = mh.poll();
+
+            long sum = curr.sum;
+            int ind = curr.index;
+
+            // i is zero-based
+            if (i >= left - 1) {
+                res = (res + sum) % MOD;
+            }
+
+            // Extend the popped subarray
+            if (ind + 1 < n) {
+                long nextsum = sum + nums[ind + 1];
+
+                mh.offer(new Node(nextsum, ind + 1));
+            }
+        }
+
+        return (int) res;
     }
 }
