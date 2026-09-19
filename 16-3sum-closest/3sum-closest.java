1dbdd4088cd10fa1e7ba0982112e6283c53450b4
Devang Choudhary <devangchoudhary71@gmail.com>
2026-09-19 15:37:39 +0530

    Time: 16 ms (92.94%) | Memory: 45.7 MB (14.88%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,29 +1,28 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int closestSum = nums[0] + nums[1] + nums[2];
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-                int currentSum = nums[i] + nums[left] + nums[right];
-
-                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
-                    closestSum = currentSum;
+        int ans=nums[0]+nums[1]+nums[2];
+        for(int i=0;i<nums.length-2;i++){
+            int l=i+1;
+            int r=nums.length-1;
+            
+            while(l<r){
+                int sum=nums[i]+nums[l]+nums[r];
+                if(Math.abs(sum-target)<Math.abs(ans-target)){
+ans=sum;
                 }
+                if(sum<target){
+                    l++;
+                }
+                else if(sum>target){
+                    r--;
 
-                if (currentSum < target) {
-                    left++;
-                } else if (currentSum > target) {
-                    right--;
-                } else {
-                    // Exact match found
-                    return currentSum;
+                }
+                else{
+                    return sum;
                 }
             }
         }
-        return closestSum;
+        return ans;
     }
 }
