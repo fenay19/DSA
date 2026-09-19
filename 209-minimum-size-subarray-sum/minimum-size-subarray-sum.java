36e9d61cb910b1bd343866ee79e6ef758b1d4200
Devang Choudhary <devangchoudhary71@gmail.com>
2026-09-19 20:31:20 +0530

    Time: 1 ms (99.80%) | Memory: 69.3 MB (47.18%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+       int ans = Integer.MAX_VALUE;
+        int i=0;
+         int sum=0;
+        for(int j=0;j<nums.length;j++){
+         
+          sum+=nums[j];
+         
+          while(sum>=target){
+              ans=Math.min(ans,j-i+1);
+             sum-=nums[i];
+             i++;
+          }
+
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+}
